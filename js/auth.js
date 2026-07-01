65b436c692585754f47e5201e031b79824a712da
aishu104 <lakshmiaiswaryaakella@gmail.com>
2026-07-01 20:55:32 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,3 +1,6 @@
+if (!localStorage.getItem("nexuscrm_user")) {
+  window.location.href = "index.html";
+}
 document.getElementById("loginForm").addEventListener("submit", function(e) {
   e.preventDefault();
 
