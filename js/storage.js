f6a4c514cc2a26ed5d57ac7197ba4066cd67178e
aishu104 <lakshmiaiswaryaakella@gmail.com>
2026-07-01 20:29:27 +0530

    Implement Storage utility for localStorage management

---
+++ js/storage.js
@@ -0,0 +1,27 @@
+const Storage = {
+  get(key) {
+    return JSON.parse(localStorage.getItem(key)) || [];
+  },
+
+  set(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+  },
+
+  add(key, item) {
+    let data = this.get(key);
+    data.push(item);
+    this.set(key, data);
+  },
+
+  update(key, id, updatedItem) {
+    let data = this.get(key);
+    data = data.map(item => item.id === id ? updatedItem : item);
+    this.set(key, data);
+  },
+
+  delete(key, id) {
+    let data = this.get(key);
+    data = data.filter(item => item.id !== id);
+    this.set(key, data);
+  }
+};
